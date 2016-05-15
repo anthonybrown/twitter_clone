b52687706246ee2c69126a174c1152dbfcaea9e4
anthonybrown <tony@tonybrown.me>
2016-05-14 21:42:27 -0400

    react component rendering

---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,16 @@
+class Main extends React.Component {
+	render() {
+		return (
+			<h1>Important content for SEO...</h1>
+		);
+	}
+}
+
+//let documentReady = () => {
+//	React.render(
+//		<Main />,
+//		document.getElementById('react')
+//	);
+//};
+//
+//$(documentReady);
